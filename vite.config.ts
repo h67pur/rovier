245ff07f9bf74d0a826acacb071944d704ce3049
h67pur <alexlunegov52@gmail.com>
2025-04-13 06:31:48 +0300

    Сборка проекта

---
+++ vite.config.ts
@@ -0,0 +1,46 @@
+import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import path from 'path';
+import tailwindcss from "@tailwindcss/vite";
+import { resolve } from 'node:path';
+import { defineConfig } from 'vite';
+
+export default defineConfig({
+    plugins: [
+        laravel({
+            input: ['resources/js/app.ts'],
+            ssr: 'resources/js/ssr.ts',
+            refresh: true,
+        }),
+        tailwindcss(),
+        vue({
+            template: {
+                transformAssetUrls: {
+                    base: null,
+                    includeAbsolute: false,
+                },
+            },
+        }),
+    ],
+    resolve: {
+        alias: {
+            '@': path.resolve(__dirname, './resources/js'),
+            'ziggy-js': resolve(__dirname, 'vendor/tightenco/ziggy'),
+        },
+    },
+    server: {
+        host: '0.0.0.0',
+        port: 5173,
+        cors: {
+            origin: [
+                'http://localhost',
+                'http://localhost:5173'
+            ],
+            credentials: true
+        },
+        hmr: {
+            host: 'localhost',
+            protocol: 'ws'
+        }
+    }
+});
